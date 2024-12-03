fcc6e8631fbe72f1655c3dacf68d76bf0b568a60
keerthireddytummalapelly <t.y.reddy19@gmail.com>
2024-12-03 15:21:50 -0500

    Chnages

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Install dependencies
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m pip install --no-dev -r requirements.txt
+
 
 # Collect static files
 python3 manage.py collectstatic --noinput
