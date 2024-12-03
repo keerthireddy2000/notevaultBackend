b02dafc8e5610c40b153acadb6749c7d6ea5ab61
keerthireddytummalapelly <t.y.reddy19@gmail.com>
2024-12-03 15:26:04 -0500

    Changes

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install dependencies
 python3 -m pip install --upgrade pip
-python3 -m pip install --no-dev -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 
 # Collect static files
